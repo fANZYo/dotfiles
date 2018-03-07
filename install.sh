1b5033bdde49b2e9c5d961493db8b278b7479295
fANZYo <www.iehl@gmail.com>
2018-03-07 09:31:02 +0000

    Refine i3 config

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
-sudo pacman -Syu
-sudo pacman -S termite neovim tmux git nodejs yaourt arc-gtk-theme zsh-theme-powerlevel9k gmrun
-chsh -s $(which zsh)
+BASEDIR=$(pwd)
 
-# Remove old dot files
-rm ~/.gitconfig
-rm ~/.config/termite/config
-rm ~/.tmux.conf
-rm ~/.local/share/tmux/theme.sh
-rm ~/.vimrc
-rm ~/.zshrc
-rm ~/.bashrc
-rm ~/.gtkrc-2.0
-rm ~/.Xresources
-rm ~/.Xmodmap
+sudo pacman -Syu
+sudo pacman -S termite neovim tmux git nodejs npm yaourt arc-gtk-theme zsh-theme-powerlevel9k gmrun ruby i3blocks
+chsh -s $(which zsh)
 
 # Vim plugins
 mkdir -p ~/.config/nvim/bundle ~/.config/nvim/autoload
@@ -39,24 +29,50 @@ git clone https://github.com/garbas/vim-snipmate.git
 git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/christoomey/vim-tmux-navigator.git
 
+# Remove old dot files
+rm ~/.gitconfig
+rm ~/.config/termite/config
+rm ~/.tmux.conf
+rm ~/.local/share/tmux/theme.sh
+rm ~/.vimrc
+rm ~/.zshrc
+rm ~/.bashrc
+rm ~/.gtkrc-2.0
+rm ~/.Xresources
+rm ~/.Xmodmap
+rm ~/.xinitrc
+
 # Vim colors
 mkdir -p ~/.config/nvim/colors
-ln ./config/nvim/colors/monokai2.vim        ~/.config/nvim/colors/monokai2.vim
+ln $BASEDIR/config/nvim/colors/monokai2.vim        ~/.config/nvim/colors/monokai2.vim
 
 # Link new dot files
-ln ./gitconfig                    ~/.gitconfig
+ln $BASEDIR/gitconfig                    ~/.gitconfig
 mkdir -p ~/.config/termite
-ln ./config/termite/config        ~/.config/termite/config
-ln ./tmux.conf                    ~/.tmux.conf
-ln ./local/share/tmux/theme.sh    ~/.local/share/tmux/theme.sh
-ln ./config/nvim/init.vim         ~/.config/nvim/init.vim
-ln ./zshrc                        ~/.zshrc
-ln ./bashrc                       ~/.bashrc
-ln ./gtkrc-2.0                    ~/.gtkrc-2.0
-ln ./Xressource                   ~/.Xresources
-ln ./Xmodmap                      ~/.Xmodmap
-ln ./xinitrc                      ~/.xinitrc
+ln $BASEDIR/config/termite/config        ~/.config/termite/config
+ln $BASEDIR/tmux.conf                    ~/.tmux.conf
+mkdir -p ~/.local/share/tmux
+ln $BASEDIR/local/share/tmux/theme.sh    ~/.local/share/tmux/theme.sh
+ln $BASEDIR/config/nvim/init.vim         ~/.config/nvim/init.vim
+ln $BASEDIR/zshrc                        ~/.zshrc
+ln $BASEDIR/bashrc                       ~/.bashrc
+ln $BASEDIR/gtkrc-2.0                    ~/.gtkrc-2.0
+ln $BASEDIR/Xresources                   ~/.Xresources
+ln $BASEDIR/Xmodmap                      ~/.Xmodmap
+ln $BASEDIR/xinitrc                      ~/.xinitrc
+ln $BASEDIR/i3blocks.conf                ~/i3blocks.conf
+ln $BASEDIR/config/i3/config             ~/.config/i3/config
 
 # Fonts
 sudo pacman -S ttf-dejavu
-yaourt ttf-monaco ttf-opensans system-san-francisco-git
+yaourt ttf-monaco
+yaourt ttf-opensans
+yaourt system-san-francisco-font-git
+
+mkdir -p ~/.config/fontconfig/conf.d
+cp $BASEDIR/config/fontconfig/conf.d/* ~/.config/fontconfig/conf.d
+
+# Github ssh key
+ssh-keygen -t rsa -b 4096 -C "www.iehl@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
