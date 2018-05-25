03e34d331557d34f36ca8fd2326969d8505bdc45
fANZYo <www.iehl@gmail.com>
2018-05-25 16:52:45 +0100

    Fix install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ BASEDIR=$(pwd)
 BASE_AUR="https://aur.archlinux.org/cgit/aur.git/snapshot"
 
 sudo pacman -Syu
-sudo pacman -S termite neovim tmux git nodejs npm yaourt arc-gtk-theme zsh-theme-powerlevel9k gmrun ruby i3blocks openssh i3-wm i3lock i3status pcmanfm
+sudo pacman -S termite neovim tmux git nodejs npm arc-gtk-theme zsh-theme-powerlevel9k gmrun ruby i3blocks openssh i3-wm i3lock i3status pcmanfm
 chsh -s $(which zsh)
 
 # Vim plugins
@@ -34,9 +34,10 @@ git clone https://github.com/christoomey/vim-tmux-navigator.git
 rm ~/.gitconfig
 rm ~/.config/termite/config
 rm ~/.config/i3/config
+rm ~/.config/nvim/init.vim
+rm ~/i3blocks.conf
 rm ~/.tmux.conf
 rm ~/.local/share/tmux/theme.sh
-rm ~/.vimrc
 rm ~/.zshrc
 rm ~/.bashrc
 rm ~/.gtkrc-2.0
@@ -73,15 +74,17 @@ curl -L -O "$BASE_AUR/system-san-francisco-font-git.tar.gz"
 curl -L -O "$BASE_AUR/ttf-monaco.tar.gz"
 curl -L -O "$BASE_AUR/ttf-opensans.tar.gz"
 
-tar -xvf google-chrome.tar.gz
-tar -xvf system-san-francisco-font-git.tar.gz
-tar -xvf ttf-monaco.tar.gz
-tar -xvf ttf-opensans.tar.gz
+tar -xvf google-chrome.tar.gz && rm google-chrome.tar.gz
+tar -xvf system-san-francisco-font-git.tar.gz && rm system-sans-francisco-font-git.tar-gz
+tar -xvf ttf-monaco.tar.gz && rm ttf-monaco.tar.gz
+tar -xvf ttf-opensans.tar.gz && rm ttf-opensans.tar.gz
 
 cd google-chrome && makepkg -si
 cd ../system-san-francisco-font-git && makepkg -si
 cd ../ttf-monaco && makepkg -si
 cd ../ttf-opensans && makepkg -si
+
+cd ../ && rm -rf google-chrome system-san-francisco-font-git ttf-monaco ttf-opensans
 
 mkdir -p ~/.config/fontconfig/conf.d
 cp $BASEDIR/config/fontconfig/conf.d/* ~/.config/fontconfig/conf.d
