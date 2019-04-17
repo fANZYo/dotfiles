3dfb20ea2ba534fe2578bf6131352bf04044ef1f
fANZYo <www.iehl@gmail.com>
2019-04-17 15:03:14 +0100

    fix fonts and xorg-server install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ BASEDIR=$(pwd)
 BASE_AUR="https://aur.archlinux.org/cgit/aur.git/snapshot"
 
 sudo pacman -Syu
-sudo pacman -S termite neovim tmux git nodejs npm arc-gtk-theme zsh-theme-powerlevel9k gmrun ruby i3blocks openssh i3-wm i3lock i3status pcmanfm ack
+sudo pacman -S xorg-xinit xorg-server xorg-xrandr ranger acpi alsa-utils termite neovim tmux git nodejs npm arc-gtk-theme zsh-theme-powerlevel9k gmrun ruby i3blocks openssh i3-wm i3lock i3status pcmanfm ack
 chsh -s $(which zsh)
 
 # Vim plugins
@@ -47,26 +47,29 @@ rm ~/.xinitrc
 
 # Vim colors
 mkdir -p ~/.config/nvim/colors
-ln $BASEDIR/config/nvim/colors/monokai2.vim        ~/.config/nvim/colors/monokai2.vim
+ln $BASEDIR/config/nvim/colors/monokai2.vim         ~/.config/nvim/colors/monokai2.vim
 
 # Link new dot files
-ln $BASEDIR/gitconfig                    ~/.gitconfig
+ln $BASEDIR/gitconfig                               ~/.gitconfig
 mkdir -p ~/.config/termite
-ln $BASEDIR/config/termite/config        ~/.config/termite/config
-ln $BASEDIR/tmux.conf                    ~/.tmux.conf
+ln $BASEDIR/config/termite/config                   ~/.config/termite/config
+ln $BASEDIR/tmux.conf                               ~/.tmux.conf
 mkdir -p ~/.local/share/tmux
-ln $BASEDIR/local/share/tmux/theme.sh    ~/.local/share/tmux/theme.sh
-ln $BASEDIR/config/nvim/init.vim         ~/.config/nvim/init.vim
-ln $BASEDIR/zshrc                        ~/.zshrc
-ln $BASEDIR/bashrc                       ~/.bashrc
-ln $BASEDIR/gtkrc-2.0                    ~/.gtkrc-2.0
-ln $BASEDIR/Xresources                   ~/.Xresources
-ln $BASEDIR/Xmodmap                      ~/.Xmodmap
-ln $BASEDIR/xinitrc                      ~/.xinitrc
-ln $BASEDIR/i3blocks.conf                ~/i3blocks.conf
-ln $BASEDIR/config/i3/config             ~/.config/i3/config
+ln $BASEDIR/local/share/tmux/theme.sh               ~/.local/share/tmux/theme.sh
+mkdir -p ~/.local/share/wallpaper
+cp $BASEDIR/local/share/wallpaper/blueDropd.jpg     ~/.local/share/wallpaper/blueDropd.jpg
+ln $BASEDIR/config/nvim/init.vim                    ~/.config/nvim/init.vim
+ln $BASEDIR/zshrc                                   ~/.zshrc
+ln $BASEDIR/bashrc                                  ~/.bashrc
+ln $BASEDIR/gtkrc-2.0                               ~/.gtkrc-2.0
+ln $BASEDIR/Xresources                              ~/.Xresources
+ln $BASEDIR/Xmodmap                                 ~/.Xmodmap
+ln $BASEDIR/xinitrc                                 ~/.xinitrc
+ln $BASEDIR/i3blocks.conf                           ~/i3blocks.conf
+ln $BASEDIR/config/i3/config                        ~/.config/i3/config
 
 # Fonts
+cp -r $BASEDIR/local/share/fonts                    ~/.local/share/
 sudo pacman -S ttf-dejavu awesome-terminal-fonts
 
 curl -L -O "$BASE_AUR/google-chrome.tar.gz"
